8427924f7b00f26f6a171afd9635ffc462c9a8f6
Saloni <115524019+Salonisaroha@users.noreply.github.com>
2023-05-08 23:24:48 +0530

    class

---
+++ class.js
@@ -0,0 +1,20 @@
+class product{
+    constructor(itemName,price,dicount,productCode){
+        this.itemName = itemName;
+        this.price = price;
+        this.discount = discount;
+        this.productCode = productCode;
+    }
+
+}
+let pencil = new product('pencil', 20, 2, 'P10')
+const Product1 = class product{
+    constructor(itemName,price,dicount,productCode){
+        this.itemName = itemName;
+        this.price = price;
+        this.discount = discount;
+        this.productCode = productCode;
+    }
+
+}
+let chair = new Product1('chair', 499, 15,'C10')
